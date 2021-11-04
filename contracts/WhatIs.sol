492fc010ac7615015ef26fb48d74be6542a44404
shaneauerbach <shaneauerbach@gmail.com>
2021-11-04 16:28:39 -0700

    adds contract

--- contracts/WhatIs.sol
+++ contracts/WhatIs.sol
@@ -8,6 +8,7 @@ contract WhatIs {
 
   // Create a counter for the number of unique Whats
   uint public whatCount;
+  uint public maxEntryBytes = 10;
 
   // Create a mapping from uint id to What
   // mapping (uint => What) public whats;
@@ -16,24 +17,40 @@ contract WhatIs {
   // mapping (uint => WhatContent) public whatContents;
 
   // Define the state a What can be in
-  enum State{Open, Voting}
+  enum State{Open, Voting, Proposed, Accepted, Rejected}
 
   // Define a what
   struct What { 
     string name;
+    State state;
+  }
+
+  struct Entry { 
+    string what;
     string content;
     State state;
-    address payable creator;
-    address payable lastContributor;
-    bool exists;
+    address proposer;
+    uint proposedTimestamp;
   }
+  
 
   // Create a bidirectional mapping between whats and ids
   mapping (uint => What) public whats;
   mapping (string => uint) public ids;
+  // Create entry mappings
+  mapping(uint => mapping(uint => Entry)) public proposedEntries;
+  mapping(uint => mapping(uint => Entry)) public acceptedEntries;
+  mapping(uint => uint) public proposedEntriesCount;
+  mapping(uint => uint) public acceptedEntriesCount;
+  // Create ownership mappings
+  mapping(uint => mapping(address => uint)) public ownership;
+  // Create votes mapping
+  mapping(uint => uint) public votes;
+  // Creates voted mapping to track who has voted
+  mapping(uint => mapping(uint => mapping(address => bool))) public voted;
 
-  // <LogCreated event: name, id>
-  event LogCreated(string name, uint id);
+  // <LogCreated event: id, name, content>
+  event LogWhatCreated(uint id, string name, string entry);
 
   // <LogFetched event: name, id>
   event LogFetched(string name, uint id);
@@ -48,31 +65,87 @@ contract WhatIs {
     _;
   }
 
+  modifier doesExist (string memory _name) { 
+    // check that What is not already in the mapping
+    require(ids[_name] != 0);
+    _;
+  }
+
+  modifier meetsLengthLimits (string memory _entry) { 
+    // check that What is not already in the mapping
+    require(bytes(_entry).length <= maxEntryBytes);
+    _;
+  }
+
+  modifier isOpen (string memory _name) { 
+    // check that What is open
+    require(whats[getWhatID(_name)].state == State.Open);
+    _;
+  }
+
+  modifier isVoting (string memory _name) { 
+    // check that What is in voting
+    require(whats[getWhatID(_name)].state == State.Voting);
+    _;
+  }
+
+  modifier isOwner (string memory _name) { 
+    // check that a sender is an owner, i.e. entitled to vote
+    require(ownership[getWhatID(_name)][msg.sender] > 0);
+    _;
+  }
+
+  modifier hasNotAlreadyVoted (string memory _name) { 
+    // check that a sender is an owner, i.e. entitled to vote
+    require(voted[getWhatID(_name)][proposedEntriesCount[getWhatID(_name)]][msg.sender] == false);
+    _;
+  }
+
+  modifier isExpired (string memory _name) { 
+    // check that a sender is an owner, i.e. entitled to vote
+    uint id = getWhatID(_name);
+    require(now > proposedEntries[id][proposedEntriesCount[id]].proposedTimestamp + 86400);
+    _;
+  }
+
   constructor() public {
     // 1. Set the owner to the transaction sender
     owner = msg.sender;
     // 2. Initialize the sku count to 0.
-    whatCount = 1;
+    whatCount = 0;
   }
 
-  function createWhat(string memory _name, string memory _content) 
+  function createWhat(string memory _name, string memory _entry) 
     public 
     doesNotExist(_name) 
     returns (bool) 
     {
-    whats[whatCount] = What({
-      name: _name,
-      content: _content,
-      state: State.Open,
-      creator: msg.sender,
-      lastContributor: msg.sender,
-      exists: true
+    // 1. Create the What
+    whats[whatCount+1].name = _name;
+    whats[whatCount+1].state = State.Open;
+    // 2. Write the entry and mark the ownership
+    ids[_name] = whatCount+1;
+    ownership[whatCount+1][msg.sender] = 1;
+    proposedEntries[whatCount+1][1] = Entry({
+      what: _name,
+      content: _entry,
+      state: State.Accepted,
+      proposer: msg.sender,
+      proposedTimestamp: now
     });
-    // 3. Increment the whatCount by 1
-    ids[_name] = whatCount;
+    acceptedEntries[whatCount+1][1] = Entry({
+      what: _name,
+      content: _entry,
+      state: State.Accepted,
+      proposer: msg.sender,
+      proposedTimestamp: now
+    });
+    proposedEntriesCount[whatCount+1] = 1;
+    acceptedEntriesCount[whatCount+1] = 1;
+    // 3. Advance the whatCount
     whatCount += 1;
     // 4. Emit the event
-    emit LogCreated(_name, whatCount);
+    emit LogWhatCreated(ids[_name],_name, _entry);
     // 5. Return true
     return true;
     }
@@ -95,6 +168,67 @@ contract WhatIs {
       whatCount += 1;
       return whatCount;
     }
+
+   function proposeEntry(string memory _name, string memory _entry)
+    public
+    doesExist(_name)
+    isOpen(_name)
+    meetsLengthLimits(_entry)
+    returns (bool)
+    {
+      uint id = getWhatID(_name);
+      proposedEntries[id][proposedEntriesCount[id]+1] = Entry({
+        what: _name,
+        content: _entry,
+        state: State.Proposed,
+        proposer: msg.sender,
+        proposedTimestamp: now
+      });
+      proposedEntriesCount[id] += 1;
+      whats[id].state = State.Voting;
+      return true;
+    }
+
+   function vote(string memory _name)
+    public
+    doesExist(_name)
+    isVoting(_name)
+    isOwner(_name)
+    hasNotAlreadyVoted(_name)
+    returns (bool)
+    {
+      uint id = getWhatID(_name);
+      voted[id][proposedEntriesCount[id]][msg.sender]=true;
+      votes[id] += ownership[id][msg.sender];
+      // If that vote is pivotal, we terminate the vote and accept the entry
+      if (votes[id] > acceptedEntriesCount[id]/2) {
+        // Accept the entry
+        proposedEntries[id][proposedEntriesCount[id]].state = State.Accepted;
+        acceptedEntries[id][acceptedEntriesCount[id]+1] = proposedEntries[id][proposedEntriesCount[id]];
+        acceptedEntriesCount[id] += 1;
+        // Put the What back in an open state
+        whats[id].state = State.Open;
+        // Credit the proposer with an ownership token
+        ownership[id][proposedEntries[id][proposedEntriesCount[id]].proposer] += 1;
+      }
+    }
+
+   function currentTime() public returns (uint256){
+        return now;
+    }
+
+   function endVote(string memory _name)
+    public 
+    isExpired(_name)
+    returns (bool)
+    {
+      uint id = getWhatID(_name);
+      proposedEntries[id][proposedEntriesCount[id]].state = State.Rejected;
+      whats[id].state = State.Open;
+      return true;
+    }
+
+
 
     // function deleteWhat(string memory _name) 
     // public returns (bool) 
--- test/what_is.test.js
+++ test/what_is.test.js
@@ -19,9 +19,9 @@ contract("WhatIs", function (accounts) {
       assert.equal(typeof instance.owner, 'function', "the contract has no owner");
     });
 
-    it("have a whatCount that starts at one", async () => {
+    it("have a whatCount that starts at zero", async () => {
       assert.equal(typeof instance.whatCount, 'function', "the contract has no whatCount");
-      assert.equal(await instance.whatCount.call(), 1, "the whatCount doesn't start at one");
+      assert.equal(await instance.whatCount.call(), 0, "the whatCount doesn't start at zero");
     });
 
     let enumState;
@@ -47,24 +47,10 @@ contract("WhatIs", function (accounts) {
     });
     
     it("have a What struct with all the right members", async () => {
-
         assert(isDefined(subjectStruct)("name"), "Struct What should have a `name` member");
         assert(isType(subjectStruct)("name")("string"), "`name` should be of type `string`");
-
-        assert(isDefined(subjectStruct)("content"), "Struct What should have a `content` member");
-        assert(isType(subjectStruct)("content")("string"), "`content` should be of type `string`");
-
         assert(isDefined(subjectStruct)("state"), "Struct What should have a `state` member");
         assert(isType(subjectStruct)("state")("State"), "`state` should be of type `State`");
-
-        assert(isDefined(subjectStruct)("creator"), "Struct What should have a `creator` member");
-        assert(isType(subjectStruct)("creator")("address"), "`creator` should be of type `address`");
-
-        assert(isDefined(subjectStruct)("lastContributor"), "Struct What should have a `lastContributor` member");
-        assert(isType(subjectStruct)("lastContributor")("address"), "`creator` should be of type `lastContributor`");
-
-        assert(isDefined(subjectStruct)("exists"), "Struct What should have an `exists` member");
-        assert(isType(subjectStruct)("exists")("bool"), "`exists` should be of type `bool`");
     });
 
     // Not sure how to write a test on whether the required mappings whats and ids exist
@@ -78,15 +64,107 @@ contract("WhatIs", function (accounts) {
     });
     it("create What correctly when it does not already exist", async () => {
         await instance.createWhat(name,content, { from: alice });
-        // This doesn't work and I'm not sure why...
-        assert(instance.whats.call(1)._name == name);
+        test_what = await instance.whats.call(1);
+        assert(test_what['name'] == name, "Names are not being set correctly.");
+        assert(test_what['state'] == WhatIs.State.Open, "State is not being set correctly.");
+    });
+    it("correctly track entries", async () => {
+      await instance.createWhat('a','a_entry1', { from: alice });
+      await instance.createWhat('b','b_entry1', { from: bob });
+      test_proposed_entry_a = await instance.proposedEntries.call(1,1);
+      test_proposed_entry_b = await instance.proposedEntries.call(2,1);
+      test_accepted_entry_a = await instance.proposedEntries.call(1,1);
+      test_accepted_entry_b = await instance.proposedEntries.call(2,1);
+      assert(test_proposed_entry_a.content == 'a_entry1', "Entries are not being mapped correctly.");
+      assert(test_proposed_entry_b.content == 'b_entry1', "Entries are not being mapped correctly.");
+      assert(test_accepted_entry_a.content == 'a_entry1', "Entries are not being mapped correctly.");
+      assert(test_accepted_entry_b.content == 'b_entry1', "Entries are not being mapped correctly.");
+    });
+    it("correctly track ownership", async () => {
+      await instance.createWhat('a','a_entry1', { from: alice });
+      await instance.createWhat('b','b_entry1', { from: bob });
+      assert(await instance.ownership.call(1,alice) == 1, "Ownership is not being mapped correctly.");
+      assert(await instance.ownership.call(2,bob) == 1, "Ownership not being mapped correctly.");
+      assert(await instance.ownership.call(1,bob) == 0, "Ownership is not being mapped correctly.");
+      assert(await instance.ownership.call(2,alice) == 0, "Ownership not being mapped correctly.");
+    });
+    it("emit a LogWhatCreated event", async () => {
+      let eventEmitted = false;
+      const tx = await instance.createWhat(name, content, { from: alice });
+      if (tx.logs[0].event == "LogWhatCreated") {eventEmitted = true;}
+      assert.equal(eventEmitted,true,"creating a What should emit a LogWhatCreated event")
+    });
+  });
+
+  describe("When getWhatCount is called, it should", () => {
+    it("return how many Whats there are", async () => {
+      assert(await instance.getWhatCount.call() == 0);
+      await instance.createWhat(name, content, { from: alice });
+      assert(await instance.getWhatCount.call() == 1);
+    });
+  });
+
+  describe("When proposeEntry is called, it should", () => {
+    it("error when What doesn't exist", async () => {
+      await catchRevert(instance.proposeEntry(name,content, { from: bob }));
+    });
+    it("error when What is in State.Voting", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,content, { from: alice });
+      await catchRevert(instance.proposeEntry(name,content, { from: bob}));
+    });
+    it("error when entry is too many bytes", async () => {
+      maxEntryBytes = await instance.maxEntryBytes.call();
+      await instance.createWhat(name,content, { from: alice });
+      await catchRevert(instance.proposeEntry(name,'a'.repeat(maxEntryBytes+1), { from: bob}));
+    });
+    it("set the What's state to Voting", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: bob });
+      test_what = await instance.whats.call(1);
+      assert(test_what['state'] == WhatIs.State.Voting);
+    });
+    it("add the proposed entry", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: bob });
+      test_entry = await instance.proposedEntries.call(1,2);
+      assert(test_entry.content == 'a');
     });
 
   });
-  describe("When someFunction is called", () => {
-    it("it should work", () => {
-        assert(true);
+
+  describe("When vote is called, it should", () => {
+    it("error when What doesn't exist", async () => {
+      await catchRevert(instance.vote(name,{ from: bob }));
     });
+    it("error when What is in State.Open", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await catchRevert(instance.vote(name, { from: alice}));
+    });
+    it("error when the sender is not an owner", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: alice });
+      await catchRevert(instance.vote(name, { from: bob}));
+    });
+    it("error when the sender has already voted", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: alice });
+      await instance.vote(name, { from: alice });
+      await catchRevert(instance.vote(name, { from: alice}));
+    });
+    it("record that the sender has voted", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: alice });
+      await instance.vote(name, { from: alice });
+      assert(await instance.voted.call(1,2,alice) == true);
+    });
+    it("add the vote to the entry", async () => {
+      await instance.createWhat(name,content, { from: alice });
+      await instance.proposeEntry(name,'a', { from: alice });
+      await instance.vote(name, { from: alice });
+      assert(await instance.votes.call(1) == 1);
+    });
+
   });
 
 
